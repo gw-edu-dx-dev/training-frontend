f25583d854b3af32d49a24a3296a208d94621829
Reina-Fujimaki <r.fujimaki202507@gmail.com>
2025-07-02 11:38:56 +0900

    feat: Training 2 機能の追加
    入力フォームと、Spring Boot APIへのPOST送信処理を追加しました。

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import HelloMessage from './components/HelloMessage';
+import { useState } from 'react';
 
 function App() {
+    const [name, setName] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    await fetch('http://localhost:8080/api/hello', {
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name }),// ← JSON にして送信
+  });
+  
+  };
+
   return (
-    <div>
-      <h1>React + Spring Boot連携</h1>
-      <HelloMessage />
+    <div style={{ padding: '2rem' }}>
+      <h1>名前を入力してください</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          placeholder="名前を入力"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">送信</button>
+      </form>
     </div>
   );
 }
