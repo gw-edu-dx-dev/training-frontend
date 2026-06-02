91ffd16f79b65f4c501709e6707c8bf47c9f2fda
AoyamaTomohiro <aotomo7771@gmail.com>
2026-06-02 14:43:04 +0900

    hello something success

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
-import HelloMessage from './components/HelloMessage';
+import React from 'react';
 
 function App() {
+  const[name, setName] = React.useState('');
+  const[message, setMessage] = React.useState('');
+  const[error, setError] = React.useState('');
+
+  const handlesubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+     if (name.trim() === '') {
+    setError('名前を入力してください');
+    setMessage('');
+    return;
+  }
+
+
+  setError('');
+
+    const response = await fetch(
+      `http://localhost:8080/api/hello?name=${name}`
+    );
+
+    const text = await response.text();
+    setMessage(text);
+  };
+
   return (
     <div>
-      <h1>React + Spring Boot連携</h1>
-      <HelloMessage />
+      <h3>名前呼び出し</h3>
+      <form onSubmit={handlesubmit}>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">送信</button>
+      </form>
+
+      {error && <p>{error}</p>}
+
+      <p>{message}</p>
+    
     </div>
   );
 }
