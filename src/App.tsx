250f8d031aefb0c63c14589e313b287189295722
AoyamaTomohiro <aotomo7771@gmail.com>
2026-06-08 17:23:24 +0900

    練習４のreact側のコードを追加し、フォームを作成しました

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,37 @@ type User = {
 function App() {
   const [users, setUsers] = React.useState<User[]>([]);
 
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [message, setMessage] = React.useState('');
+  const [error, setError] = React.useState('');
+
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+
+    if (name.trim() === '') {
+  setError('名前を入力してください');
+  return;
+}
+
+    const response = await fetch(
+      'http://localhost:8080/api/users',
+      {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, email }),
+      }
+    )
+  const data = await response.text();
+
+  setError('');
+  setMessage(data);
+  };
+  
   //非同期処理をasync/awaitで行っている
   React.useEffect(() => {
     const getUsers = async () => {
@@ -22,6 +53,30 @@ function App() {
 
   return (
     <>
+<h1>ユーザー登録画面</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+        type="text"
+        placeholder="名前"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        />
+        <input
+        type="email"
+        placeholder="メールアドレス"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit">
+          登録
+          </button>
+      </form>
+
+
+      <p>{error}</p>
+      <p>{message}</p>
+
+
       <h1>User一覧画面</h1>
 
       <ul>
